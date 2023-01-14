35fb77aa10126ad2719d8f0259692e824ad0d118
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-01-14 20:44:30 -0300

    Falta rutas

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
+import HomeContainer from './components/home/HomeContainer';
 import NavBarContainer from './components/navBar/NavBarContainer';
+import style from './app.module.css';
 
 function App() {
   return (
-    <div>
-      <NavBarContainer />
+    <div className={style.videoGamesContainer__div}>
+      <div className={style.videoGamesContainer__navBarContainer}>
+        <NavBarContainer />
+      </div>
+      <div className={style.videoGamesContainer__homeContainer}>
+        <HomeContainer />
+      </div>
     </div>
   );
 }
