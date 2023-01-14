7a7565217af1e8b8e6780391d157541e272dfc62
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-01-14 16:37:49 -0300

    Se agregó img para el NavBar

--- client/src/components/navBar/NavBarHome.jsx
+++ client/src/components/navBar/NavBarHome.jsx
@@ -12,11 +12,8 @@ const NavBarHome = () => {
       <h3>Messages</h3>
       <h3>Developments</h3>
       <h3>Matches</h3>
-      <h3>Score</h3>
-      <h3>Tasks</h3>
+      <h3>Store</h3>
       <h3>Favorite</h3>
-      <h3>Tournaments</h3>
-      <h3>Balance</h3>
       <h3>Suggestions</h3>
       <NavBarSocialMedia />
       <h3>Help</h3>
--- client/src/components/navBar/NavBarLogo.jsx
+++ client/src/components/navBar/NavBarLogo.jsx
@@ -4,7 +4,7 @@ import style from './style/NavBarLogo.module.css';
 const NavBarLogo = () => {
   return (
     <div className={style.div_logo}>
-      <img src={logo} alt='Violet joystick' height='100em' width='100em' />
+      <img src={logo} alt='Violet joystick' height='90em' width='90em' />
       <h2 className={style.h2_logo}>
         <b>Markish</b>
       </h2>
