cbeb5970982a798b4c754b6764525a5c24c0b924
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-01-14 22:14:44 -0300

    Se agregó rutas

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
+import style from './app.module.css';
+import { Route } from 'react-router-dom';
 import HomeContainer from './components/home/HomeContainer';
 import NavBarContainer from './components/navBar/NavBarContainer';
-import style from './app.module.css';
+import GamesContainer from './components/games/GamesContainer';
 
 function App() {
   return (
@@ -8,9 +10,16 @@ function App() {
       <div className={style.videoGamesContainer__navBarContainer}>
         <NavBarContainer />
       </div>
-      <div className={style.videoGamesContainer__homeContainer}>
-        <HomeContainer />
-      </div>
+      <Route exact path='/'>
+        <div className={style.videoGamesContainer__homeContainer}>
+          <HomeContainer />
+        </div>
+      </Route>
+      <Route exact path='/games'>
+        <div className={style.videoGamesContainer__homeContainer}>
+          <GamesContainer />
+        </div>
+      </Route>
     </div>
   );
 }
---
+++ client/src/components/games/GamesContainer.jsx
@@ -0,0 +1,4 @@
+const GamesContainer = () => {
+  return <div>GamesContainer</div>;
+};
+export default GamesContainer;
