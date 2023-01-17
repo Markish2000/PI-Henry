6e4d66a73ca80a51865d02c2f261bbc918335e4a
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-01-16 22:44:41 -0300

    Se agregó estilos

--- client/src/components/games/GamesCard.jsx
+++ client/src/components/games/GamesCard.jsx
@@ -1,10 +1,16 @@
+import style from './style/GamesCard.module.css';
+import imgRating from './assets/rating.png';
+
 const GamesCard = ({ id, name, image, genres, rating, platforms }) => {
   return (
-    <div key={id}>
+    <div key={id} className={style.gamesCard__div}>
+      <div>
+        <h3>{rating}</h3>
+        <img src={imgRating} alt='Rating' width='20em' />
+      </div>
       <h2>{name}</h2>
-      <img src={image} alt={name} />
+      <img src={image} alt={name} width='100em' />
       <h3>{genres}</h3>
-      <h3>{rating}</h3>
       <h3>{platforms}</h3>
     </div>
   );
