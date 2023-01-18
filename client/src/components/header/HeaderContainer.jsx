65c8403d251f67a485f4f99fe69746780ec365d9
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-01-18 03:31:21 -0300

    Se completó el header

--- client/src/components/header/HeaderContainer.jsx
+++ client/src/components/header/HeaderContainer.jsx
@@ -1,21 +1,35 @@
 import style from './style/HeaderContainer.module.css';
 import notification from './assets/notification.png';
 import perfil from './assets/perfil.jpeg';
+import search from './assets/search.png';
 
 const HeaderContainer = () => {
   return (
     <div className={style.headerContainer__div}>
       <div className={style.headerContainer__div__input}>
+        <img
+          src={search}
+          alt='Search'
+          className={style.headerContainer__img_input}
+        />
         <input
           type='text'
           placeholder='Search...'
           className={style.headerContainer__input}
         />
+        <img
+          src={notification}
+          alt='Notification'
+          className={style.headerContainer__notification}
+        />
       </div>
-      <div>
-        <h5>Marcos Parella</h5>
-        <img src={perfil} alt='prop.name' width='100em' />
-        <img src={notification} alt='Notification' width='20em' />
+      <div className={style.headerContainer__div_h5_img}>
+        <h5 className={style.headerContainer__div_h5}>Marcos Parella</h5>
+        <img
+          src={perfil}
+          className={style.headerContainer__div_img}
+          alt='prop.name'
+        />
       </div>
     </div>
   );
