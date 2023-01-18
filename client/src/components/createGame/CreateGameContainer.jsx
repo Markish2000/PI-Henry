428078d7e4159d463552f90ef2821738989ee855
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-01-18 03:41:23 -0300

    Idea en createGame

--- client/src/components/createGame/CreateGameContainer.jsx
+++ client/src/components/createGame/CreateGameContainer.jsx
@@ -1,3 +1,8 @@
+import React from 'react';
+
+// TODO: Cuando el usuario vaya creando el video game aparecerá una visualización de como quedaría.
+// ?? ¿Debería ir guardando en un estado local y hacer que se renderice la app cada vez que hay un cambio en el estado?
+
 const CreateGameContainer = () => {
   return (
     <div>
