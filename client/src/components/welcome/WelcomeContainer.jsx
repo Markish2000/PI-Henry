9dd757b11a098568cbb114566754c221852dfd04
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-01-26 03:21:32 -0300

    Se arregló best games

--- client/src/components/welcome/WelcomeContainer.jsx
+++ client/src/components/welcome/WelcomeContainer.jsx
@@ -1,8 +1,16 @@
 import WelcomeForm from './WelcomeForm';
 import WelcomeVideo from './WelcomeVideo';
 import style from './style/WelcomeContainer.module.css';
+import { useDispatch } from 'react-redux';
+import { getAllGames } from '../../redux/actions';
+import { useEffect } from 'react';
 
 const WelcomeContainer = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllGames());
+  }, [dispatch]);
+
   return (
     <div className={style.welcomeContainer__div}>
       <WelcomeForm />
